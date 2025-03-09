065ac6b77768520a02daa156a148bb612df4f94e
sidak <gdsd1524@gmail.com>
2025-03-10 03:58:38 +0530

    .

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-
 export const axiosInstance = axios.create({
-  baseURL: "https://insi-chat-backend.vercel.app/",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://insi-chat-backend.vercel.app/api",
   withCredentials: true,
 });
